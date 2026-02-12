103c047e15f8a430011aff570b68ddafb63d504d
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2026-02-12 12:04:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,36 +8,7 @@ import os
 
 markdown_contents = {}
 note_paths = {
-    "topics0001": "notes/topics0001.md",
-    "topics0002": "notes/topics0002.md",
-    "topics0003": "notes/topics0003.md",
-    "dem_topics0001": "DEM/topics0001.md",
-    "mdpd_refs": "MDPD/refs.md",
-    "ioz_topics0001": "IOZ/topics0001.md",
-    "ioz_topics0002": "IOZ/topics0002.md",
-    "ioz_topics0003": "IOZ/topics0003.md",
-    "pfm_topics0001": "PFM/topics0001.md",
-    "mlip_install": "MLIP/install.md",
-    "deepmd_dplr": "DEEPMD/dplr.md",
-    "aenet_install": "AENET/aenet_install.md",
-    "aenet_lammps": "AENET/aenet_lammps.md",
-    "reaxff_install": "LAMMPS/reaxff_install.md",
-    "fix_bond_react": "LAMMPS/fix_bond_react.md",
-    "fix_bond_react_key": "LAMMPS/fix_bond_react_key.md",
-    "volume_fraction": "COLLOID/volume_fraction.md",
-    "eff_volume_fraction": "COLLOID/eff_volume_fraction.md",
-    "kd_viscosity": "COLLOID/kd_viscosity.md",
-    "gel_intro": "COLLOID/gel_intro.md",
-    "dpd_Pan2010": "COLLOID/dpd_Pan2010.md",
-    "kapsel_install": "KAPSEL/kapsel_install.md",
-    "regular_solution": "ATAT/regular_solution.md",
-    "bragg_williams_enthalpy": "ATAT/bragg_williams_enthalpy.md",
-    "ideal_mixture_entropy": "ATAT/ideal_mixture_entropy.md",
-    "sqs": "ATAT/sqs.md",
-    "mcsqs": "ATAT/mcsqs.md",
-    "sqs_quality": "ATAT/sqs_quality.md",
-    "delta_function_lorentzian": "MATH/delta_function_lorentzian.md",
-    "replica_trick": "MATH/replica_trick.md",
+    "mdpd_fundamental": "MDPD/mdpd_fundamental.md",
 }
 
 for key, path in note_paths.items():
