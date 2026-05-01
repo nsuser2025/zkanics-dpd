b90b94b820919092168ef10133b02ec4bb7354df
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2026-05-01 09:10:47 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,16 +4,36 @@ import streamlit.components.v1 as components
 import json
 import os
 
-#st.set_page_config(layout="wide")　最大化
+st.set_page_config(layout="wide")
+st.markdown("""
+<style>
+.block-container {
+    max-width: 900px;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
+""", unsafe_allow_html=True)
 
 markdown_contents = {}
 note_paths = {
-    "mdpd_fundamental": "MDPD/mdpd_fundamental.md",
-    "other_dpds": "MDPD/other_dpds.md",
-    "trimesh_cylinder": "MESH2LAMMPS/trimesh_cylinder.md",
-    "trimesh": "MESH2LAMMPS/trimesh.md",
-    "mesh2lammps": "MESH2LAMMPS/mesh2lammps.md",
-    "mdpd_refs": "MDPD/refs.md",
+     "chapter001": "notes/chapter001.md",
+     "chapter002": "notes/chapter002.md",
+     "chapter003": "notes/chapter003.md",
+     "topics004": "notes/topics004.md",
+     "topics005": "notes/topics005.md",
+     "sfgmd": "notes/sfgmd.md",
+     "non_condon_effect": "notes/non_condon_effect.md",
+     "rotation_matrix_01": "notes/rotation_matrix_01.md", 
+     "rotation_matrix_02": "notes/rotation_matrix_02.md",
+     "window": "notes/window.md",
+     "artifact_in_3200": "notes/artifact_in_3200.md",
+     "nma01": "notes/nma01.md",
+     "nma02": "notes/nma02.md",
+     "nma03": "notes/nma03.md",
+     "nma04": "notes/nma04.md",
+     "nma05": "notes/nma05.md",
+     "nma06": "notes/nma06.md",
 }
 
 for key, path in note_paths.items():
